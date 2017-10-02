23fb839cd0e64e139679acb1661baa381bba06af
Gustav Rödström <gustav.rodstrom@gmail.com>
2017-10-02 18:57:55 +0200

    IO

--- Solutions/src/AutoCorrect.java
+++ Solutions/src/AutoCorrect.java
@@ -1,3 +1,11 @@
+import IO.Reader;
+import IO.Writer;
+
+import java.util.List;
 
 public class AutoCorrect {
+    public static void main(String[] args) {
+        List<String> input = new Reader().readInput();
+        new Writer().writeOutput(input);
+    }
 }
--- Solutions/src/IO/Reader.java
+++ Solutions/src/IO/Reader.java
@@ -8,9 +8,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Reader {
-
     private ArrayList<String> contents = new ArrayList<>();
+    private String line = "";
 
+    /**
+     * Read a whole file into an ArrayList<String>
+     * @param filename
+     * @return
+     */
     public ArrayList<String> readFile(String filename) {
         try {
             return read(new FileReader(filename));
@@ -21,24 +26,50 @@ public class Reader {
         }
     }
 
+    /**
+     * Reads from System.in until it gets an empty line.
+     * @return
+     */
     public ArrayList<String> readInput () {
         return read(new InputStreamReader(System.in));
     }
 
-    public ArrayList<String> getLastRead() {
-        return contents;
+    /**
+     * Read a single line from System.in
+     * @return
+     */
+    public String readInputLine () {
+        return readLine(new InputStreamReader(System.in));
     }
 
     private ArrayList<String> read (InputStreamReader in) {
         contents = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(in)) {
             String line;
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null && !line.equals("")) {
                 contents.add(line);
             }
         } catch (IOException e) {
             System.out.println("Could not read input \n" + e);
         }
         return contents;
+    }
+
+    private String readLine (InputStreamReader in) {
+        line = "";
+        try (BufferedReader br = new BufferedReader(in)) {
+            line = br.readLine();
+        } catch (IOException e) {
+            System.out.println("Could not read input \n" + e);
+        }
+        return line;
+    }
+
+    public ArrayList<String> getLastRead() {
+        return contents;
+    }
+
+    public String getLastReadLine() {
+        return line;
     }
 }
