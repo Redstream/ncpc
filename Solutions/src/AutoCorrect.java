2f7d7ea024f0b1795fec5dc8dcb1632bfb80656a
Gustav Rödström <gustav.rodstrom@gmail.com>
2017-10-02 23:27:04 +0200

    stuff & stuff

--- Solutions/src/AutoCorrect.java
+++ Solutions/src/AutoCorrect.java
@@ -1,45 +1,63 @@
-import sun.reflect.generics.tree.Tree;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class AutoCorrect {
 
-    private  long dictionaryWords;
-    private  long wordsToType;
-    private  Scanner sc;
-    private  TreeNode dictionary;
+    private long dictionaryWords;
+    private long wordsToType;
+    private Scanner sc;
+    private TreeNode dictionary;
 
     public static void main(String[] args) {
         new AutoCorrect().compute();
     }
 
-    public void compute() {
+    private void compute() {
         sc = new Scanner(System.in);
         dictionaryWords = sc.nextLong();
         wordsToType = sc.nextLong();
         createDictionary();
+
+        for (int i = 0; i < wordsToType; i++) {
+            System.out.println(shortestPath(sc.nextLine()));
+        }
     }
 
-    public void createDictionary() {
+    private void createDictionary() {
         dictionary = new TreeNode(null, Character.MIN_VALUE);
         for(int i = 0; i < dictionaryWords; i++) {
             String word = sc.nextLine();
             TreeNode currentNode = dictionary;
             for(Character c : word.toCharArray()) {
-                TreeNode newNode = new TreeNode(currentNode, c);
-                currentNode = newNode;
+                currentNode = new TreeNode(currentNode, c);
             }
         }
     }
 
+    // TODO: 2017-10-02
+    private int shortestPath(String word) {
+        TreeNode endNode = getEndNode(word);
+        TreeNode startNode = dictionary;
+        return 0;
+    }
+
+    private TreeNode getEndNode(String word) {
+        TreeNode endNode = dictionary;
+        for (Character c : word.toCharArray()) {
+            if (endNode.children.get(c) == null) {
+                return endNode;
+            }
+            endNode = endNode.children.get(c);
+        }
+        return endNode;
+    }
+
     class TreeNode {
-        private TreeNode parent;
-        private Character c;
-        private HashMap<Character, TreeNode> children = new HashMap<>();
-        private TreeNode prioChildren;
-        private String preString;
+        public TreeNode parent;
+        public Character c;
+        public HashMap<Character, TreeNode> children = new HashMap<>();
+        public TreeNode prioChildren;
+        public String preString;
 
         TreeNode(TreeNode parent, Character c){
             this.parent = parent;
@@ -50,10 +68,6 @@ public class AutoCorrect {
             } else {
                 preString = c.toString();
             }
-        }
-
-        public TreeNode getChildrenAtCharacter(Character c) {
-            return children.get(c);
         }
 
         public void addChildren(TreeNode child) {
@@ -70,7 +84,7 @@ public class AutoCorrect {
                  return prioChildren.getTabWord();
              }
         }
-    }
 
+    }
 
 }
